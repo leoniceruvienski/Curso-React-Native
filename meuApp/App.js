e580f8858440ef53cc22476e7ae444f620ff38c5
Leonice <leonice@pop-os.localdomain>
2022-02-25 15:17:19 -0300

    Criado Botão de Login

--- meuApp/App.js
+++ meuApp/App.js
@@ -1,42 +1,31 @@
 import React, { Component} from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Button} from 'react-native';
 
 class App extends Component{
-  render(){
 
-    let nome = 'Leonice';
-   
+  constructor(props){
+    super(props);
+    this.state= {
+      nome: ''
+    };
 
-    return(
-      <View>
-        <Text>Boa tarde</Text>
-        <Text>Meu Primeiro App</Text>
-        <Text style ={{ color: 'red', fontSize: 25, margin: 15 }}>
-          Sujeito Programador
-        </Text>
-
-        <Text style={{ fontSize: 30 }}> {nome} </Text>
-
-        <Jobs largura={300} altura={300} nome="STEVE JOOBS"/>
-
-      </View>
-    );
+    this.entrar = this.entrar.bind(this);
   }
-}
 
-class Jobs extends Component{
+  entrar(nome){
+    this.setState({
+      nome: nome
+    })
+  }
+  
   render(){
 
-    let img = 'https://sujeitoprogramador.com/steve.png'
     return(
-      <View>
-          <Image
-           source={{ uri: img } }
-           style={{ width: this.props.largura, height: this.props.altura }}
-          />
-          <Text>{this.props.nome}</Text>
+      <View style={{ marginTop: 20}}>
+        <Button title="Entrar" onPress={ () => this.entrar('Leonice Ruvienski')}/>
+        <Text style={{fontSize: 25, color: 'red', textAlign: 'center'}}>{this.state.nome}</Text>
+  
       </View>
-    
     );
   }
 }
