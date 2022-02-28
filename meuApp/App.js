57642e4495c320754976b7cd453ae3288f7e6264
Leonice <leonice@pop-os.localdomain>
2022-02-28 10:35:47 -0300

    Recebendo Dados

--- meuApp/App.js
+++ meuApp/App.js
@@ -1,28 +1,59 @@
 import React, { Component} from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: 'Leonice'
+    };
+
+    this.pegaNome = this.pegaNome.bind(this);
+  }
+
+  pegaNome(texto){
+    if(texto.length > 0){ 
+      this.setState({nome: 'Bem Vindo:'+ texto});
+   }else{
+    this.setState({nome: ''})
+   }
+   
+  }
 
   render(){
 
     return(
-      <View style={{flex:1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'flex-start'}}>
+    <View style={styles.container}>
 
-        <View style={{width: 50,height: 50, backgroundColor: 'green'}}>
-         </View>
+      <TextInput
+      style={styles.input}
+      placeholder= "Digite seu Nome"
+      underlineColorAndroid="transparente"
+      onChangeText={this.pegaNome}
+      />
+      <Text style={styles.texto}>{this.state.nome}</Text>
 
-        <View style={{width: 50, height: 50,backgroundColor: 'red'}}>
-        </View>
-
-        <View style={{ width: 50, height: 50, backgroundColor: 'yellow'}}></View>
-
-        <View style={{width: 50,height: 50, backgroundColor: 'green'}}>
-         </View>
-       
-    
-      </View>
+    </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  input:{
+    height: 45,
+    borderWidth: 45,
+    borderColor: 'red',
+    margin: 10,
+    fontSize: 30,
+   
+  },
+  texto:
+  {
+    textAlign: 'center',
+    fontSize: 25,
+  }
+});
 
 export default App;
