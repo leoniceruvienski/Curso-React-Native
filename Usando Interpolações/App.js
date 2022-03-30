d55db91be19a2ff0ccc23e99782f9b0d48ac3bf9
Leonice <leonice@pop-os.localdomain>
2022-03-30 11:06:46 -0300

    Interpolação

---
+++ Usando Interpolações/App.js
@@ -0,0 +1,52 @@
+import React, { Component} from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
+
+export default class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+
+      LarAnimada: new Animated.Value(0)
+      
+    };
+
+    Animated.timing(
+      this.state.LarAnimada,
+      {
+        toValue: 100,
+        duration: 3000
+      }
+    )
+      
+  }
+  
+  render() {
+
+    let porcentagemAnimada = this.state.LarAnimada.interpolate({
+      inputRange: [0, 100],
+      outputRange: ['0%', '100%']
+    });
+
+    return(
+      <View style={styles.container}>
+
+        <Animated.View style={{backgroundColor: '#4169e1', width: porcentagemAnimada, height: 25}}>
+
+        </Animated.View>
+
+      
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ddd',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
